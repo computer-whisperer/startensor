f1df2b151e91125084ea2d0f27127d6fe2a1d556
Christian J. Balcom <robot.inventor@gmail.com>
2017-11-03 03:16:14 -0400

    updated replaysorter (no longer works with python 2, but now multithreaded!)

--- replaysorter.py
+++ replaysorter.py
@@ -1,26 +1,63 @@
 from mpyq import MPQArchive
 import json
 import os
+import queue
+import threading
+
 
 def sort_file(filename):
+   # file = open(filename, 'r')
     archive = MPQArchive(filename)
     jsondat = archive.read_file("replay.gamemetadata.json")
+   # file.close()
     dat = json.loads(jsondat)
     winner = None
     looser = None
+
     for player in dat["Players"]:
+        if player is None:
+            continue
         if player["Result"] == "Win":
             winner = player
-        else:
+        if player["Result"] == "Loss":
             looser = player
-    if not (winner is not None and looser is not None and "MMR" in winner and "MMR" in looser):
-        os.renames(filename, "naughty/{}".format(filename))
-    new_fname = "{}/{}/{}/{}/{}".format(winner["AssignedRace"], looser["AssignedRace"], winner["MMR"] - winner["MMR"]%100, looser["MMR"] - looser["MMR"]%100, filename)
-    os.renames(filename, new_fname)
+        if "MMR" not in player:
+            player["MMR"] = 0
+
+    if dat["Duration"] < 20:
+        os.renames(filename, "short/{}".format(filename))
+    elif winner is None or looser is None:
+        os.renames(filename, "missingplayer/{}".format(filename))
+    else:
+        new_fname = "{}/{}/{}/{}/{}".format(winner["AssignedRace"], looser["AssignedRace"], winner["MMR"] - winner["MMR"]%100, looser["MMR"] - looser["MMR"]%100, filename)
+        os.renames(filename, new_fname)
+
+def worker():
+    while True:
+        item = q.get()
+        if item is None:
+            break
+        try:
+            sort_file(item)
+        except Exception as e:
+            print(e)
+        q.task_done()
+
+q = queue.Queue()
+threads = []
+for i in range(4):
+    t = threading.Thread(target=worker)
+    t.start()
+    threads.append(t)
 
 for filename in os.listdir("."):
     if filename.endswith(".SC2Replay"):
-        try:
-            sort_file(filename)
-        except Exception as e:
-            print(e)
+        q.put(filename)
+
+q.join()
+
+for t in threads:
+    q.put(None)
+
+for t in threads:
+    t.join()
